0ae214d87b8f1cc2fabca991867cff6c940763fc
ryujh030820 <ryujh030820@gmail.com>
2024-06-15 19:30:50 +0900

    next.config.mjs 수정 (사진 뜨도록)

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["d12zq4w4guyljn.cloudfront.net", "lh5.googleusercontent.com", "*"],
+        domains: ["d12zq4w4guyljn.cloudfront.net", "lh5.googleusercontent.com", "search.pstatic.net", "*"],
     },
     reactStrictMode: true,
 };
