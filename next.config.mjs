7b88ffdab3e1e7e8bb2a2fb4bdfd1899d5ac760f
jongyoung3 <jongyong5645@gmail.com>
2024-06-17 14:06:18 +0900

    이미지 경로 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["d12zq4w4guyljn.cloudfront.net", "lh5.googleusercontent.com", "search.pstatic.net", "*"],
+        domains: ["d12zq4w4guyljn.cloudfront.net", "lh5.googleusercontent.com", "search.pstatic.net", 
+            "place.map.kakao.com", "mblogthumb-phinf.pstatic.net","postfiles.pstatic.net",
+            "img.restaurantguru.com","naverbooking-phinf.pstatic.net","img1.daumcdn.net",
+            "*"],
     },
     reactStrictMode: true,
 };
