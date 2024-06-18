d1d561f9e34f1d9d0b51c415de43d8ac460de723
jongyoung3 <jongyong5645@gmail.com>
2024-06-18 11:31:03 +0900

    리뷰 평균 별점계산 로직 효율개선 및 매개변수 타입 변경

--- src/app/detail/[id]/_lib/calculateAverageStarRate.ts
+++ src/app/detail/[id]/_lib/calculateAverageStarRate.ts
@@ -1,20 +1,20 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { ChungstaurantFirestore } from "@/firebase";
 
 // 특정 음식점에 대한 리뷰 데이터의 starRate 평균을 계산
-export default async function calculateAverageStarRate(restaurantId: string) {
+export default async function calculateAverageStarRate(restaurantId: number) {
+    // 특정 restaurantId에 대한 리뷰만 쿼리
     const reviewCollection = collection(ChungstaurantFirestore, 'ReviewListData');
-    const querySnapshot = await getDocs(reviewCollection);
+    const restaurantQuery = query(reviewCollection, where("restaurantId", "==", restaurantId));
+    const querySnapshot = await getDocs(restaurantQuery);
 
     let totalStarRate = 0;
     let reviewCount = 0;
 
     querySnapshot.forEach((doc) => {
         const data = doc.data();
-        if (data.restaurantId === restaurantId) {
-            totalStarRate += data.starRate;
-            reviewCount += 1;
-        }
+        totalStarRate += data.starRate;
+        reviewCount += 1;
     });
 
     return reviewCount === 0 ? 0 : totalStarRate / reviewCount;
