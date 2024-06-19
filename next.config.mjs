28eb25ebfa60fae537d345865c29e226a08f0d00
ryujh030820 <ryujh030820@gmail.com>
2024-06-19 20:14:23 +0900

    CORS 문제 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,52 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["d12zq4w4guyljn.cloudfront.net", "lh5.googleusercontent.com", "search.pstatic.net", 
-            "place.map.kakao.com", "mblogthumb-phinf.pstatic.net","postfiles.pstatic.net",
-            "img.restaurantguru.com","naverbooking-phinf.pstatic.net","img1.daumcdn.net", "lh3.googleusercontent.com", "img1.kakaocdn.net",
-            "modo-phinf.pstatic.net", "firebasestorage.googleapis.com",
-            "*"],
+        domains: [
+            "d12zq4w4guyljn.cloudfront.net",
+            "lh5.googleusercontent.com",
+            "search.pstatic.net",
+            "place.map.kakao.com",
+            "mblogthumb-phinf.pstatic.net",
+            "postfiles.pstatic.net",
+            "img.restaurantguru.com",
+            "naverbooking-phinf.pstatic.net",
+            "img1.daumcdn.net",
+            "lh3.googleusercontent.com",
+            "img1.kakaocdn.net",
+            "modo-phinf.pstatic.net",
+            "firebasestorage.googleapis.com",
+            "*",
+        ],
+    },
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                    },
+                ],
+            },
+        ];
+    },
+
+    webpack(config, options) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            issuer: { and: [/\.(js|ts)x?$/] },
+            use: ["@svgr/webpack"],
+        });
+
+        return config;
     },
     reactStrictMode: true,
 };
---
+++ src/app/pages/_document.tsx
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var script = document.createElement('script');
+              script.src = '//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&libraries=services&autoload=false';
+              document.body.appendChild(script);
+            `,
+          }}
+        />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
